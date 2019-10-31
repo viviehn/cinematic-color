a661494b96a653ee45bb4b72354611104b41ec5f
bmild <cecilia77@berkeley.edu>
2019-10-30 22:39:51 -0700

    add test

--- data_utils.py
+++ data_utils.py
@@ -1,7 +1,8 @@
 import numpy as np
-import json
+import json, csv
 from PIL import Image
 
+# get rid of the black boundaries if it's a 16:9 movie trailer
 def crop_frame(image, cropx=None, cropy=None):
     if image.shape[0] == 720 and image.shape[1] == 1280:
         image = image[100:600, ...]
@@ -10,7 +11,7 @@ def crop_frame(image, cropx=None, cropy=None):
 def load_json(metadata_path, csv_path):
     with open(metadata_path, 'r') as jsonfile:
         metadata_all = json.load(jsonfile)
-    metadata = dict{}
+    metadata = dict()
     with open(csv_path, 'r') as csvfile: 
         yt_data = csv.reader(csvfile)
         line_count = 0
@@ -23,7 +24,15 @@ def load_json(metadata_path, csv_path):
                 line_count += 1
                 key = int(float(row[1]))
                 yt_filename = row[4]
-                metadata['%s'%key] = metadata_all['%s'%key]
-                metadata['%s'%key].update('yt_filename': yt_filename)
+                try:
+                    metadata['%s'%key] = metadata_all['%s'%key]
+                except Exception as e:
+                    print('cannot get key %s'%e)
+                    continue
+                metadata['%s'%key].update({'yt_filename': yt_filename})
     return metadata
   
+metadata_path = 'movie_details.json'
+csv_path = 'trailers.csv'
+meta = load_json(metadata_path, csv_path)
+# print(meta)
